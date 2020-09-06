ae7761deff4e5c0a15c3bd13dd05e894c0a64683
rodolphe37 <rodolphe.a@gmail.com>
2020-09-06 20:40:22 +0200

    add template for cra

--- template/src/index.js
+++ template/src/index.js
@@ -1,6 +1,5 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import swDev from './serviceworkerDev'
