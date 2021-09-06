e8f79213887c3d80184f7afb0a74d39573ba2a75
rodolphe37 <rodolphe.a@gmail.com>
2021-09-06 10:31:31 +0200

    chart language & update react version from 16.13.… 1 to 17.0.2

--- template/src/components/dashBoard/AllLanguagesDashboard.js
+++ template/src/components/dashBoard/AllLanguagesDashboard.js
@@ -1,41 +1,53 @@
-import React, { useEffect } from 'react';
-import JavascriptChart from './allLanguagesDashboard/JavascriptChart';
-import JavaChart from './allLanguagesDashboard/JavaChart';
-import PhpChart from './allLanguagesDashboard/PhpChart';
-import PythonChart from './allLanguagesDashboard/PythonChart';
-import RubyChart from './allLanguagesDashboard/RubyChart';
-import CplusPlusChart from './allLanguagesDashboard/CplusPlusChart';
-import TypescriptChart from './allLanguagesDashboard/TypescriptChart';
-import HtmlChart from './allLanguagesDashboard/HtmlChart';
-import CssChart from './allLanguagesDashboard/CssChart';
-import DartChart from './allLanguagesDashboard/DartChart';
-import SwiftChart from './allLanguagesDashboard/SwiftChart';
-import KotlinChart from './allLanguagesDashboard/KotlinChart'
-import VueChart from './allLanguagesDashboard/VueChart';
-import axios from 'axios';
-import { useStateValue } from '../../StateProvider';
-import { actionTypes } from '../../reducer';
-
+import React, { useEffect, useState } from "react";
+import {
+  JavascriptChart,
+  JavaChart,
+  PhpChart,
+  PythonChart,
+  RubyChart,
+  CplusPlusChart,
+  TypescriptChart,
+  HtmlChart,
+  CssChart,
+  DartChart,
+  SwiftChart,
+  KotlinChart,
+  VueChart,
+} from "./allLanguagesDashboard/";
+import axios from "axios";
+import { useStateValue } from "../../StateProvider";
+import { actionTypes } from "../../reducer";
+import { useTranslation } from "react-i18next";
 
 const AllLanguagesDashboard = () => {
   // eslint-disable-next-line
-  const [{ }, dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
+  const [mode, setMode] = useState("online");
+  const { t } = useTranslation();
 
   useEffect(() => {
-    axios.get(`https://raw.githubusercontent.com/madnight/githut/master/src/data/gh-pull-request.json`)
-      .then(res => {
+    axios
+      .get(
+        `https://raw.githubusercontent.com/madnight/githut/master/src/data/gh-pull-request.json`
+      )
+      .then((res) => {
+        console.log(res);
         dispatch({
           type: actionTypes.SET_DATA,
-          Data: res.data
-        })
+          Data: res.data,
+        });
       })
-      .catch((error) => alert(error.message))
-
-  }, [dispatch])
+      .catch((error) => setMode("offline") && alert(error.message));
+  }, [dispatch]);
 
   return (
-    < React.Fragment >
+    <React.Fragment>
       <div>
+        {mode === "offline" ? (
+          <div className=" alert alert-danger" role="alert">
+            {t("alert")}
+          </div>
+        ) : null}
         <div>
           <div className="javascript">
             <JavascriptChart />
@@ -84,7 +96,7 @@ const AllLanguagesDashboard = () => {
           <br />
         </div>
       </div>
-    </React.Fragment >
-  )
-}
-export default AllLanguagesDashboard
+    </React.Fragment>
+  );
+};
+export default AllLanguagesDashboard;
